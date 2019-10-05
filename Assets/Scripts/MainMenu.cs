3b43dc403d5dc032b44cb41ecd83d54be5e82d73
Niels Bernlhr <kormarun@gmail.com>
2019-10-05 19:42:40 +0200

    Main Menu & Level transitions

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject root;
+
+    private const String FIRST_STAGE = "First Stage";
+    private const String SECOND_STAGE = "Second Stage";
+    private const String CREDITS_STAGE = "Credits Stage";
+
+    public static int choice;
+
+    private void HideAllStages()
+    {
+        root.transform.Find(FIRST_STAGE).gameObject.SetActive(false);
+        root.transform.Find(SECOND_STAGE).gameObject.SetActive(false);
+        root.transform.Find(CREDITS_STAGE).gameObject.SetActive(false);
+    }
+
+    private void ShowStage(String name)
+    {
+        HideAllStages();
+        root.transform.Find(name).gameObject.SetActive(true);
+    }
+
+    public void OnClickPlay()
+    {
+        ShowStage(SECOND_STAGE);
+    }
+
+    public void ShowCredits()
+    {
+        ShowStage(CREDITS_STAGE);
+    }
+
+
+    public void PlayArmor()
+    {
+        choice = 0;
+        LoadMainScene();
+    }
+
+    private static void LoadMainScene()
+    {
+        SceneManager.LoadScene("TestNiels");
+    }
+
+    public void PlaySword()
+    {
+        choice = 1;
+        LoadMainScene();
+    }
+
+    public void PlayNothing()
+    {
+        choice = 2;
+        LoadMainScene();
+    }
+
+    public void GoBackCredits()
+    {
+        ShowStage(FIRST_STAGE);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/PlayerSetup.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSetup : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        switch (MainMenu.choice)
+        {
+            case 0:
+                // TODO: Spawn Reaper
+                // TODO: Spawn Player with armor
+                break;
+            case 1:
+                // TODO: Spawn Reaper
+                // TODO: Spawn Player with sword
+                break;
+            case 2:
+                // TODO: Spawn Player with naked
+                break;
+            default:
+                break;
+        }
+    }
+}
