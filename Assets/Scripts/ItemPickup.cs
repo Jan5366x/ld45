5097f65a7e5ab0071cd30afbb80ba567393385e9
Niels Bernlhr <kormarun@gmail.com>
2019-10-05 14:35:38 +0200

    Item pickup and more prefabs

---
+++ Assets/Scripts/ItemPickup.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPickup : MonoBehaviour
+{
+    public GameObject prefab;
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Player player = other.gameObject.GetComponent<Player>();
+        if (player)
+        {
+            GameObject.Instantiate(prefab, other.transform);
+            Destroy(gameObject);
+        }
+    }
+}
