e49e053f2d0a0acc6b8aa127111ede436ec85ea6
Niels Bernlhr <kormarun@gmail.com>
2019-10-05 18:37:29 +0200

    Spawner

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -5,23 +5,13 @@ public class HealthBar : MonoBehaviour
     public float width = 200;
     public float height = 20;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-    }
-
     private void OnGUI()
     {
         Entity entity = transform.parent.GetComponent<Entity>();
         if (entity)
         {
             var pos = Camera.main.WorldToScreenPoint(transform.position);
-            Rect rect = new Rect(pos.x - 100, Camera.main.pixelHeight - pos.y, width, height);
+            Rect rect = new Rect(pos.x - width / 2, Camera.main.pixelHeight - pos.y, width, height);
             IMUIHelper.DrawFilledBorderRect(rect, 1, entity.health / (float) entity.maxHealth, Color.black,
                 Color.green);
         }
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public SpawnerWave[] waves;
+    public bool playerInRange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (var wave in waves)
+        {
+            StartCoroutine("SpawnPickups", wave);
+        }
+    }
+
+    IEnumerator SpawnPickups(SpawnerWave item)
+    {
+        yield return new WaitForSeconds(Random.Range(item.delayMin, item.delayMax));
+        if (playerInRange)
+        {
+            SpawnAround(item);
+        }
+    }
+
+    void SpawnAround(SpawnerWave wave)
+    {
+        int count = Random.Range(wave.countMin, wave.countMax);
+        for (int i = 0; i < count; i++)
+        {
+            var position = transform.position;
+            var range = wave.spawnerRange;
+            float spawnX = Random.Range(position.x - range.x, position.x + range.x);
+            float spawnY = Random.Range(position.y - range.y, position.y + range.y);
+
+            Instantiate(wave.prefab, new Vector3(spawnX, spawnY), Quaternion.identity);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.GetComponent<Player>())
+        {
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.GetComponent<Player>())
+        {
+            playerInRange = false;
+        }
+    }
+}
