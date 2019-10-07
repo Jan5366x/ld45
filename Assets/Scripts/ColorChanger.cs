b3e3a266451c019e3b63315858e5309f9989c748
Niels Bernlhr <kormarun@gmail.com>
2019-10-07 15:46:12 +0200

    Killer slime & Death reason

---
+++ Assets/Scripts/ColorChanger.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class ColorChanger : MonoBehaviour
+{
+    public float updateDuration;
+    public float updateCounter;
+
+    // Update is called once per frame
+    void Update()
+    {
+        updateCounter -= Time.deltaTime;
+        if (updateCounter < 0)
+        {
+            SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+            if (spriteRenderer)
+            {
+                spriteRenderer.color = Random.ColorHSV();
+                updateCounter = updateDuration;
+            }
+        }
+    }
+}
