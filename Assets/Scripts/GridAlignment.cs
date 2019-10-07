31b98ea17b89d666963e8a95a34992babf51a7a9
Niels Bernlhr <kormarun@gmail.com>
2019-10-07 20:56:10 +0200

    Actual map & grid alignment improvements

--- Assets/Scripts/GridAlignment.cs
+++ Assets/Scripts/GridAlignment.cs
@@ -1,18 +1,27 @@
+using System;
 using UnityEngine;
 
 [ExecuteInEditMode]
 public class GridAlignment : MonoBehaviour
 {
-    [Range(1, 1000)] public float stepsPerUnitX = 100;
-    [Range(1, 1000)] public float stepsPerUnitY = 100;
+    [Range(1, 1000)] public double stepsPerUnitX = 100;
+    [Range(1, 1000)] public double stepsPerUnitY = 100;
 
+
+    private void Awake()
+    {
+        if (Application.isPlaying)
+        {
+            enabled = false;
+        }
+    }
 
     [ContextMenu("Align")]
     public void Align()
     {
         Vector3 position = transform.position;
-        position.x = Mathf.Floor(position.x * stepsPerUnitX) / stepsPerUnitX;
-        position.y = Mathf.Floor(position.y * stepsPerUnitY) / stepsPerUnitY;
+        position.x = (float) (Math.Round(position.x * stepsPerUnitX) / stepsPerUnitX);
+        position.y = (float) (Math.Round(position.y * stepsPerUnitY) / stepsPerUnitY);
         position.z = 0;
 
         transform.position = position;
@@ -24,8 +33,8 @@ public class GridAlignment : MonoBehaviour
         if (spriteRenderer)
         {
             Vector2 size = GetComponent<SpriteRenderer>().size;
-            stepsPerUnitX = 1 / size.x;
-            stepsPerUnitY = 1 / size.y;
+            stepsPerUnitX = 1d / (double) size.x;
+            stepsPerUnitY = 1d / (double) size.y;
         }
 
         Align();
