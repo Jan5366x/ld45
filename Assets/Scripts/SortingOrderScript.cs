2b06ac027f4d73d50060a06d5f6a793c81bffba6
Jan5366x <jan5366@gmail.com>
2019-10-07 11:53:42 +0200

    Work on sorting order script

--- Assets/Scripts/SortingOrderScript.cs
+++ Assets/Scripts/SortingOrderScript.cs
@@ -1,24 +1,59 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
 public class SortingOrderScript : MonoBehaviour
 {
-    public float offset;
+    private const int ChildLayerSpace = 100;
 
-    void Awake()
+    public float groundOffset;
+
+    private int _sortingOrder = 0;
+    private SpriteRenderer _spriteRenderer;
+    private bool _hasOwnRenderer = false;
+ 
+
+    private void Awake()
     {
+        Prepare();
         SetPosition();
     }
 
     void Update()
     {
         SetPosition();
+        
+        var objPos = transform.position;
+        
+        Debug.DrawLine(
+            new Vector3(objPos.x + -0.3F,objPos.y + groundOffset), 
+            new Vector3(objPos.x + 0.3F,objPos.y + groundOffset),
+            _hasOwnRenderer ? Color.yellow : Color.magenta);
     }
 
     void SetPosition()
     {
-        GetComponent<SpriteRenderer>().sortingOrder = (int) -((transform.position.y + offset) * 100);
+        
+        // update sprite renderer if in Editor mode to provide live updates
+        if(!Application.isPlaying)
+            Prepare();
+        
+        _sortingOrder = (int) -((transform.position.y + groundOffset) * ChildLayerSpace);
+        
+        if (_hasOwnRenderer)
+            _spriteRenderer.sortingOrder = _sortingOrder;
+    }
+
+    private void Prepare()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _hasOwnRenderer = _spriteRenderer != null;
+    }
+
+    public int GetSortingOrder()
+    {
+        return _sortingOrder;
     }
 }
